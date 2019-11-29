dd7e4800f956c37356a3d773ac21475a66a514ea
stekern <stekern@users.noreply.github.com>
2019-11-29 18:51:53 +0100

    Backend: Prettier

--- beatmatch-server/helpers/logic.js
+++ beatmatch-server/helpers/logic.js
@@ -1,34 +1,34 @@
-import cosineSimilarity from "compute-cosine-similarity";
+import cosineSimilarity from 'compute-cosine-similarity'
 
 /* Calculate cosine similarity between two sets of aggregated audio features */
 function getSimilarity(firstAudioFeatures, secondAudioFeatures) {
   const [s1, s2] = [firstAudioFeatures, secondAudioFeatures].map(p =>
     Object.values(p)
-  );
-  return cosineSimilarity(s1, s2);
+  )
+  return cosineSimilarity(s1, s2)
 }
 
 const getAggregatedAudioFeatures = playlist => {
   const keys = [
-    "danceability",
-    "energy",
-    "speechiness",
-    "acousticness",
-    "instrumentalness",
-    "liveness",
-    "valence"
-  ]; // 'tempo'
+    'danceability',
+    'energy',
+    'speechiness',
+    'acousticness',
+    'instrumentalness',
+    'liveness',
+    'valence'
+  ] // 'tempo'
   const aggregatedAudioFeatures = playlist.tracks.reduce((acc, curr) => {
     keys.map(key => {
       if (key in acc) {
-        acc[key] += curr.audioFeatures[key] / playlist.tracks.length;
+        acc[key] += curr.audioFeatures[key] / playlist.tracks.length
       } else {
-        acc[key] = curr.audioFeatures[key] / playlist.tracks.length;
+        acc[key] = curr.audioFeatures[key] / playlist.tracks.length
       }
-    });
-    return acc;
-  }, {});
-  return aggregatedAudioFeatures;
-};
+    })
+    return acc
+  }, {})
+  return aggregatedAudioFeatures
+}
 
-export { getSimilarity, getAggregatedAudioFeatures };
+export { getSimilarity, getAggregatedAudioFeatures }
--- beatmatch-server/helpers/spotify.js
+++ beatmatch-server/helpers/spotify.js
@@ -1,16 +1,16 @@
-const Spotify = require("node-spotify-api");
-const config = require("../config.js");
+const Spotify = require('node-spotify-api')
+const config = require('../config.js')
 const spotify = new Spotify({
   id: config.spotify.client_id,
   secret: config.spotify.secret
-});
+})
 
-const TRACK_LIMIT = 100;
+const TRACK_LIMIT = 100
 
 const getTracks = async (playlistId, offset) => {
   const res = await spotify.request(
     `${config.spotify.baseUrl}/playlists/${playlistId}/tracks?offset=${offset}`
-  );
+  )
   const tracks = res.items
     .filter(t => !t.track.is_local)
     .map(t => ({
@@ -18,9 +18,9 @@ const getTracks = async (playlistId, offset) => {
       artists: t.track.artists.map(a => a.name),
       title: t.track.name,
       id: t.track.id
-    }));
-  return tracks;
-};
+    }))
+  return tracks
+}
 
 /* Returns the tracks in a playlist, batched in lists of 100 tracks (the TRACK_LIMIT)
  * E.g., a playlist with 100 tracks returns [ [ Track, Track, Track, ... ] ]
@@ -29,32 +29,32 @@ const getTracks = async (playlistId, offset) => {
 const getPlaylistWithTracks = async playlistId => {
   const playlistInfo = await spotify.request(
     `${config.spotify.baseUrl}/playlists/${playlistId}`
-  );
-  const numTracks = playlistInfo.tracks.total;
+  )
+  const numTracks = playlistInfo.tracks.total
 
-  let tracklists = [await getTracks(playlistId, 0)];
+  let tracklists = [await getTracks(playlistId, 0)]
   if (numTracks > TRACK_LIMIT) {
-    const numRequests = Math.ceil((numTracks - TRACK_LIMIT) / TRACK_LIMIT);
+    const numRequests = Math.ceil((numTracks - TRACK_LIMIT) / TRACK_LIMIT)
     const results = await Promise.all(
       Array(numRequests)
         .fill()
         .map((_, i) => getTracks(playlistId, (i + 1) * TRACK_LIMIT))
-    );
-    tracklists = [...tracklists, ...results];
+    )
+    tracklists = [...tracklists, ...results]
   }
-  return tracklists;
-};
+  return tracklists
+}
 
 /* Returns a list of all tracks in a playlist accompanied by their audio features and general details */
 const getPlaylistWithAudioFeatures = async playlistId => {
-  const playlist = await getPlaylistWithTracks(playlistId);
+  const playlist = await getPlaylistWithTracks(playlistId)
   const tracksWithAudioFeatures = await Promise.all(
     playlist.map(async tracklist => {
-      const trackIdsString = tracklist.map(t => t.id).join(",");
+      const trackIdsString = tracklist.map(t => t.id).join(',')
       const audioFeaturesRaw = await spotify.request(
         `${config.spotify.baseUrl}/audio-features/?ids=${trackIdsString}`
-      );
-      const audioFeatures = audioFeaturesRaw["audio_features"].map(t => ({
+      )
+      const audioFeatures = audioFeaturesRaw['audio_features'].map(t => ({
         danceability: t.danceability,
         energy: t.energy,
         speechiness: t.speechiness,
@@ -62,18 +62,18 @@ const getPlaylistWithAudioFeatures = async playlistId => {
         instrumentalness: t.instrumentalness,
         liveness: t.liveness,
         valence: t.valence
-      }));
+      }))
       return audioFeatures.map((t, i) => ({
         audioFeatures: t,
         ...tracklist[i]
-      }));
+      }))
     })
-  );
+  )
   const flattened = tracksWithAudioFeatures.reduce((acc, curr) => [
     ...acc,
     ...curr
-  ]);
-  return { tracks: flattened };
-};
+  ])
+  return { tracks: flattened }
+}
 
-export { spotify, getPlaylistWithAudioFeatures as getPlaylist };
+export { spotify, getPlaylistWithAudioFeatures as getPlaylist }
