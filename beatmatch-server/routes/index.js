ba5c0707ceeaedc723914515dc37189557df6c89
Stein-Aage Klaussen <steinaage@Stein-Aages-MacBook-Pro-2.local>
2019-11-29 15:26:13 +0100

    Mye tull med callbacks

--- beatmatch-server/routes/index.js
+++ beatmatch-server/routes/index.js
@@ -14,26 +14,117 @@ router.use(cors({
 }));
 
 
-/* GET songs for playlist. */
-router.get('/playlist/:playlist_id', function(req, res, next) {
+
+
+
+function splitArrayIntoChunks(array) {
+  console.log("array size")
+  console.log(array.length)
+
+  var i,j,temparray,chunk = 100;
+  var returnarray = [];
+  for (i=0,j=array.length; i<j; i+=chunk) {
+      temparray = array.slice(i,i+chunk);
+      returnarray.push(temparray);
+  }
+  console.log("returnarray")
+  return returnarray;
+}
+
+
+function getPlaylistTrackItems(playlist_id, offset, cb_done) {
+
+  // get the items in the playlist based on offsets, e.g. from 0-99 and 100-136
+  var getPlaylistTrackItemsUrl = `${config.spotify.baseUrl}/playlists/${playlist_id}/tracks?offset=${offset * 100}`;
+  console.log(getPlaylistTrackItemsUrl);
+
   spotify
-  .request(`${config.spotify.baseUrl}/playlists/${req.params.playlist_id}`)
+  .request(getPlaylistTrackItemsUrl)
   .then(function(tracks) {
-    
-    var track_ids = getTrackIds(tracks.tracks.items);
-    var track_ids_string = track_ids.join(",");
-    spotify
-    .request(`${config.spotify.baseUrl}/audio-features/?ids=${track_ids_string}`)
-    .then(function(audio_features) {
-      var mergedSongs = mergeSongsWithAudioFeatures(tracks.tracks.items, audio_features.audio_features);
-      return res.send(mergedSongs);
+    // track_items is either 100 or 36
+    var track_items = tracks.items;
+    console.log("track items length")
+    console.log(track_items.length)
+    track_items = track_items.slice(offset * 100);
+    var track_batches = splitArrayIntoChunks(track_items);
+
+
+    // get audio features in chunks of 100
+    var track_items_all = [];
+    async.forEach(track_batches, function(track_items, cb) {
+      getAudioFeaturesForTracks(track_items, function(err, mergedSongs) {
+        if (err) {cb(err);}
+        console.log("mergedSongs")
+        console.log(mergedSongs.length)
+        track_items_all = track_items_all.concat(mergedSongs);
+        cb();
+      })
+    }, function(err) {
+      if (err) {throw err;}
+      cb_done(null, track_items_all);
     })
-    .catch(function(err) {
-      console.error('Error occurred: ' + err); 
-    });
   })
   .catch(function(err) {
-    console.error('Error occurred: ' + err); 
+    cb_done(err);
+  });
+}
+
+function getPlaylist(playlist_id, cb_done) {
+  spotify
+  .request(`${config.spotify.baseUrl}/playlists/${playlist_id}`)
+  .then(function(tracks) {
+    var playlist_data = {};
+    playlist_data["total"] = tracks.tracks.total;
+    console.log(tracks.tracks.total)
+    console.log("playlist_data")
+
+    console.log(playlist_data)
+    cb_done(null, playlist_data);
+  })
+  .catch(function(err) {
+    cb_done(err);
+  });
+}
+
+function getOffsetsFromTotal(total) {
+  var offsets = [];
+  var totalAmounts = Math.ceil(total/100);
+  
+  for (var i = 0; i < totalAmounts; i++) {
+    offsets.push(i);
+  } 
+  return offsets;
+}
+
+
+/* GET songs for playlist. */
+router.get('/playlist/:playlist_id', function(req, res, next) {
+
+  getPlaylist(req.params.playlist_id, function(err, playlist_data) {
+    if (err) {throw err;}
+
+    // Get offsets for lenght of arraylist e.g. offsets for 136 is [0, 1]
+    var offsets = getOffsetsFromTotal(playlist_data.total);
+    console.log("total number of tracks: ", playlist_data.total)
+
+    var track_items_all = [];
+    async.forEach(offsets, function(offset, cb) {
+      getPlaylistTrackItems(req.params.playlist_id, offset, function(err, track_items) {
+        if (err) {cb(err);}
+        track_items_all = track_items_all.concat(track_items);
+        cb();
+      });
+    }, function(err) {
+      console.log("total number of non-local tracks: ", track_items_all.length)
+      if (err) {throw err;}
+      return res.send({
+        metadata: playlist_data,
+        tracks: track_items_all
+      });
+    });
+
+
+    
   });
 });
 
@@ -58,12 +149,10 @@ router.get('/song/features/:songid', function(req, res, next) {
 });
 
 function mergeSongsWithAudioFeatures(track_items, audio_features) {
-  console.log(track_items[0]);
-
   var mergedSongs = [];
   for (var i = 0; i < track_items.length; i++) {
     for (var j = 0; j < audio_features.length; j++) {
-      if (track_items[i].track.id == audio_features[j].id) {
+      if (audio_features[j] && track_items[i].track && (track_items[i].track.id == audio_features[j].id)) {
 
         var newTrackItem = {};
         newTrackItem["popularity"] = track_items[i].track.popularity;
@@ -82,11 +171,27 @@ function mergeSongsWithAudioFeatures(track_items, audio_features) {
   return mergedSongs;
 }
 
+function getAudioFeaturesForTracks(track_items, cb) {
+  var track_ids = getTrackIds(track_items);
+  var track_ids_string = track_ids.join(",");
+  spotify
+  .request(`${config.spotify.baseUrl}/audio-features/?ids=${track_ids_string}`)
+  .then(function(audio_features) {
+    var mergedSongs = mergeSongsWithAudioFeatures(track_items, audio_features.audio_features);
+    cb(null, mergedSongs);
+  })
+  .catch(function(err) {
+    cb(err);
+  });
+}
+
 function getTrackIds(track_items) {
   var track_ids = [];
   for (var i = 0; i < track_items.length; i++) {
-    var track_id = track_items[i].track.id;
-    track_ids.push(track_id);
+    if (track_items[i].track) {
+      var track_id = track_items[i].track.id;
+      track_ids.push(track_id);
+    }
   }
   return track_ids;
 }
