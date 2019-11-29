dca81e03dc34ed4b1e0e2cdc2ba92e3e358fb966
Julian Jark <juliuxu@gmail.com>
2019-11-29 16:01:35 +0100

    add PlaylistAverageAudioFeatures

--- beatmatch-frontend/src/types.ts
+++ beatmatch-frontend/src/types.ts
@@ -1,1 +1,12 @@
 export interface Playlist {}
+
+export interface PlaylistAverageAudioFeatures {
+  danceability: number
+  energy: number
+  speechiness: number
+  acousticness: number
+  instrumentalness: number
+  liveness: number
+  valence: number
+  tempo: number
+}
