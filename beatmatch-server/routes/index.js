90b8b326bee017ab372879bcbe300aa13f3175ea
stekern <stekern@users.noreply.github.com>
2019-11-29 18:51:53 +0100

    Backend: Prettier

--- beatmatch-server/routes/index.js
+++ beatmatch-server/routes/index.js
@@ -1,35 +1,35 @@
-import express from "express";
-import cors from "cors";
-import { getPlaylist } from "../helpers/spotify.js";
-import { getAggregatedAudioFeatures, getSimilarity } from "../helpers/logic.js";
+import express from 'express'
+import cors from 'cors'
+import { getPlaylist } from '../helpers/spotify.js'
+import { getAggregatedAudioFeatures, getSimilarity } from '../helpers/logic.js'
 
-const router = express.Router();
+const router = express.Router()
 router.use(
   cors({
-    origin: "*"
+    origin: '*'
   })
-);
+)
 
 /* GET songs for playlist. */
-router.get("/playlist/:playlistId", async (req, res, next) => {
-  const playlist = await getPlaylist(req.params.playlistId);
-  const aggregatedAudioFeatures = getAggregatedAudioFeatures(playlist);
+router.get('/playlist/:playlistId', async (req, res, next) => {
+  const playlist = await getPlaylist(req.params.playlistId)
+  const aggregatedAudioFeatures = getAggregatedAudioFeatures(playlist)
   return res.send({
     aggregatedAudioFeatures,
     tracks: playlist.tracks
-  });
-});
+  })
+})
 
 /* GET similarity score for two playlists */
-router.get("/similarity", async (req, res, next) => {
-  const ids = [req.query.firstPlaylistId, req.query.secondPlaylistId];
+router.get('/similarity', async (req, res, next) => {
+  const ids = [req.query.firstPlaylistId, req.query.secondPlaylistId]
   const [p1, p2] = await Promise.all(
     ids.map(async id => getAggregatedAudioFeatures(await getPlaylist(id)))
-  );
-  const similarity = getSimilarity(p1, p2);
+  )
+  const similarity = getSimilarity(p1, p2)
   return res.send({
     similarity
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
