219a210bbfef1e4ed1f8ccf71f236cef57fcb191
Julian Jark <juliuxu@gmail.com>
2019-11-29 18:44:47 +0100

    prettier fix

--- beatmatch-server/config.js
+++ beatmatch-server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	spotify: {
-		baseUrl: "https://api.spotify.com/v1",
-		client_id: "***REMOVED***",
-		secret: "***REMOVED***"
-	}
-}
+  spotify: {
+    baseUrl: "https://api.spotify.com/v1",
+    client_id: "***REMOVED***",
+    secret: "***REMOVED***"
+  }
+};
