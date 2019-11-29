0b8583643f95479db8253d18cb662ca8886998d3
Hans Christian Alsos <hcalsos@gmail.com>
2019-11-29 15:35:09 +0100

    stuff

--- beatmatch-frontend/src/App.js
+++ beatmatch-frontend/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import InputWithButton from "./components/InputWithButton"
-import './App.css';
+import React from 'react'
+import InputWithButton from './components/InputWithButton'
+import AveragePlaylistBarWithInput from './components/AveragePlaylistBarWithInput'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <InputWithButton buttonText="Test"/>
+      <AveragePlaylistBarWithInput />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- beatmatch-frontend/src/components/InputWithButton.jsx
+++ beatmatch-frontend/src/components/InputWithButton.jsx
@@ -1,29 +1,32 @@
-import React, { useState } from 'react';
-import { getAudioFeaturesForPlaylist } from "../utils/api";
+import React, { useState } from 'react'
+import { getAudioFeaturesForPlaylist } from '../utils/api'
 
 const InputWithButton = props => {
-    const [inputId, setInputId] = useState("");
+  const [inputId, setInputId] = useState('')
 
-    const handleInputChange = event => {
-        setInputId(event.target.value)
-    }
+  const handleInputChange = event => {
+    setInputId(event.target.value)
+  }
 
-    const handleButtonClick = async event => {
-        event.preventDefault();
-        const res = await getAudioFeaturesForPlaylist(inputId);
-    }
+  const handleButtonClick = async (event, props) => {
+    event.preventDefault()
+    const res = await getAudioFeaturesForPlaylist(inputId)
+    props.setPlaylist(res)
+  }
 
-    return (
-        <div>
-            <input 
-                type="text"
-                placeholder="Playlist id"
-                value={inputId}
-                onChange={handleInputChange}
-            />
-            <button onClick={handleButtonClick}>{props.buttonText}</button>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Playlist id"
+        value={inputId}
+        onChange={handleInputChange}
+      />
+      <button onClick={event => handleButtonClick(event, props)}>
+        {props.buttonText}
+      </button>
+    </div>
+  )
 }
 
-export default InputWithButton;
+export default InputWithButton
