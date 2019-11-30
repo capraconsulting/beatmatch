b38e2dab1648100635c9eb6ded2e311a077c9c7a
Julian Jark <juliuxu@gmail.com>
2019-11-30 11:41:09 +0100

    cleanup grid

--- beatmatch-frontend/src/App.tsx
+++ beatmatch-frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import './App.css'
 import { Playlist, getMockPlaylist } from './types'
 import PlaylistSelector from './components/PlaylistSelector'
-import { Divider, Grid } from '@material-ui/core'
+import { Grid, Container } from '@material-ui/core'
 import Simularity from './components/Simularity'
 import PlaylistVisualizer from './components/charts/PlaylistVisualizer'
 
@@ -18,44 +17,48 @@ function App() {
   )
 
   return (
-    <div className="App">
-      <Grid container>
-        <Grid item>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
           <PlaylistSelector
             buttonText="Sett spilleliste 1"
             onSelect={res => setPlaylist1(res)}
           />
         </Grid>
-        <Grid item>
+        <Grid item xs={6}>
           <PlaylistSelector
             buttonText="Sett spilleliste 1"
             onSelect={res => setPlaylist2(res)}
           />
         </Grid>
-      </Grid>
-      <Divider />
 
-      {!playlist1 && !playlist2 && <div>Select a playlist</div>}
+        {!playlist1 && !playlist2 && (
+          <Grid item xs={12}>
+            Select a playlist
+          </Grid>
+        )}
 
-      {playlist1 && playlist2 && (
-        <div>
-          <Simularity {...{ playlist1, playlist2 }} />
-          <Divider />
-        </div>
-      )}
-      <Grid container>
+        {playlist1 && playlist2 && (
+          <Grid item xs={12}>
+            <Simularity {...{ playlist1, playlist2 }} />
+          </Grid>
+        )}
         {playlist1 && (
-          <Grid item>
-            <PlaylistVisualizer playlist={playlist1} />
+          <Grid item xs={playlist1 && playlist2 ? 6 : 12}>
+            <div style={{ outline: '2px solid black ' }}>
+              <PlaylistVisualizer playlist={playlist1} />
+            </div>
           </Grid>
         )}
         {playlist2 && (
-          <Grid item>
-            <PlaylistVisualizer playlist={playlist2} />
+          <Grid item xs={playlist1 && playlist2 ? 6 : 12}>
+            <div style={{ outline: '2px solid black ' }}>
+              <PlaylistVisualizer playlist={playlist2} />
+            </div>
           </Grid>
         )}
       </Grid>
-    </div>
+    </Container>
   )
 }
 
