39349a55cf67882d46c739ce87fd6c23d9a20bf9
Julian Jark <juliuxu@gmail.com>
2019-11-30 11:18:14 +0100

    add mocks

--- beatmatch-frontend/src/App.tsx
+++ beatmatch-frontend/src/App.tsx
@@ -1,29 +1,60 @@
 import React from 'react'
 import './App.css'
-import { Playlist } from './types'
+import { Playlist, getMockPlaylist } from './types'
 import PlaylistSelector from './components/PlaylistSelector'
-import { Divider } from '@material-ui/core'
+import { Divider, Grid } from '@material-ui/core'
 import Simularity from './components/Simularity'
 import PlaylistVisualizer from './components/charts/PlaylistVisualizer'
 
+const USE_MOCK1 = true
+const USE_MOCK2 = true
+
 function App() {
-  const [playlist1, setPlaylist1] = React.useState<Playlist | null>(null)
-  const [playlist2, setPlaylist2] = React.useState<Playlist | null>(null)
+  const [playlist1, setPlaylist1] = React.useState<Playlist | null>(
+    USE_MOCK1 ? getMockPlaylist() : null
+  )
+  const [playlist2, setPlaylist2] = React.useState<Playlist | null>(
+    USE_MOCK2 ? getMockPlaylist() : null
+  )
 
   return (
     <div className="App">
-      <PlaylistSelector
-        buttonText="Playlist 1"
-        onSelect={res => setPlaylist1(res)}
-      />
-      <PlaylistSelector
-        buttonText="Playlist 2"
-        onSelect={res => setPlaylist2(res)}
-      />
+      <Grid container>
+        <Grid item>
+          <PlaylistSelector
+            buttonText="Sett spilleliste 1"
+            onSelect={res => setPlaylist1(res)}
+          />
+        </Grid>
+        <Grid item>
+          <PlaylistSelector
+            buttonText="Sett spilleliste 1"
+            onSelect={res => setPlaylist2(res)}
+          />
+        </Grid>
+      </Grid>
       <Divider />
-      {playlist1 && playlist2 && <Simularity />}
-      {playlist1 && <PlaylistVisualizer playlist={playlist1} />}
-      {playlist2 && <PlaylistVisualizer playlist={playlist2} />}
+
+      {!playlist1 && !playlist2 && <div>Select a playlist</div>}
+
+      {playlist1 && playlist2 && (
+        <div>
+          <Simularity {...{ playlist1, playlist2 }} />
+          <Divider />
+        </div>
+      )}
+      <Grid container>
+        {playlist1 && (
+          <Grid item>
+            <PlaylistVisualizer playlist={playlist1} />
+          </Grid>
+        )}
+        {playlist2 && (
+          <Grid item>
+            <PlaylistVisualizer playlist={playlist2} />
+          </Grid>
+        )}
+      </Grid>
     </div>
   )
 }
--- beatmatch-frontend/src/components/Simularity.tsx
+++ beatmatch-frontend/src/components/Simularity.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Playlist } from '../types'
 /**
  * TODO
  * two playlist ids
  * Show score
  * Update on playlist id change
  */
-export default () => (
-  <div>
-    <pre>
-      * TODO: Two playlist ids * Show score * Update on playlist id change
-    </pre>
-  </div>
-)
+
+interface Props {
+  playlist1: Playlist
+  playlist2: Playlist
+}
+
+const Simularity = ({ playlist1, playlist2 }: Props) => {
+  console.log(playlist1, playlist2)
+  return (
+    <div>
+      <pre>
+        * TODO: Two playlist ids * Show score * Update on playlist id change
+      </pre>
+    </div>
+  )
+}
+
+export default Simularity
