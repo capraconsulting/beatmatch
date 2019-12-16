2f94c1bbe7552ea353e6f865a2a8ec7204496f7f
stekern <stekern@users.noreply.github.com>
2019-12-16 08:35:57 +0100

    Use dotenv for keys

--- beatmatch-server/config.js
+++ beatmatch-server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   spotify: {
     baseUrl: 'https://api.spotify.com/v1',
-    client_id: '***REMOVED***',
-    secret: '***REMOVED***'
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    secret: process.env.SPOTIFY_SECRET
   }
 }
